7b090a1d95e2fe26ccc6ec74cb11038150b8933c
Jardel Martins <jardelvdm@gmail.com>
2018-06-20 09:59:08 -0300

    [qanchaka] atualizando a exibição da pontuação atual, seleção da carta baseada na etapa atual e a atualização da pontuação do jogador em todas as categorias que estão nas cartas

--- quanchaka/js/carta.js
+++ quanchaka/js/carta.js
@@ -45,7 +45,15 @@ quanchaka.controller('carta',['$scope','tools', function(scope,tools){
 
   scope.jogador = {
     xp:0,
-    etapa:0
+    etapa:0,
+    categorias: [
+      {nome: 'Saúde', pontos:0 },
+      {nome: 'Financeiro', pontos:0 },
+      {nome: 'Profissional', pontos:0 },
+      {nome: 'Psicológico', pontos:0 },
+      {nome: 'Relações', pontos:0 },
+      {nome: 'Ética', pontos:0 }
+    ]
   }
 
   scope.etapas = [
@@ -57,42 +65,53 @@ quanchaka.controller('carta',['$scope','tools', function(scope,tools){
   scope.etapa = scope.etapas[scope.jogador.etapa];
 
   scope.atualizaProgressoJogador = () => {
+    // atualiza os pontos de cada categoria
+    scope.carta.categorias.forEach(c => {
+      const categoria = scope.jogador.categorias.find(jc => jc.nome == c.nome)
+      const etapaAtual = scope.etapas[scope.jogador.etapa].nome;
+      categoria.pontos += c[etapaAtual].pontos
+    })
+
+    // atualiza o progresso geral no jogo
     scope.jogador.xp += scope.carta.xp;
     scope.jogador.etapa = scope.etapas.filter(etapa => etapa.xp <= scope.jogador.xp).length - 1;
 
     scope.etapa = scope.etapas[scope.jogador.etapa];
-    console.log('scope.jogador', scope.jogador)
+
+    // console.log('scope.jogador', scope.jogador);
   }
 
   scope.sorteiaCarta = () => {
-    const cartasNaoUsadas = scope.cartas.filter(c => c.usada == false);
+    // filtra as cartas não usadas e que tenham pontuações na fase atual
+    const cartasNaoUsadas = scope.cartas
+      .filter(carta => carta.usada == false)
+      .filter(carta => carta.categorias)
+      .filter(carta => carta.categorias.some(cat => cat[scope.etapa.nome].pontos > 0));
     const cartasNaoUsadasLength = cartasNaoUsadas.length;
     const numeroAleatorio = randomNumber(0,cartasNaoUsadasLength); // Baseado No Limite de cartas disponível
     scope.carta = cartasNaoUsadas[numeroAleatorio];
     
-    scope.atualizaProgressoJogador();
+    if (scope.carta) {
+      scope.atualizaProgressoJogador();
 
-    console.log('carta',scope.carta)
-
-    if(!scope.carta.categoria || scope.carta.categoria == "Saúde"){
-      scope.carta.classe = 'saude';
-      scope.carta.categoria = "Saúde";
-    } else if(scope.carta.categoria == "Financeiro"){
+      if (!scope.carta.categoria || scope.carta.categoria == "Saúde") {
+        scope.carta.classe = 'saude';
+        scope.carta.categoria = "Saúde";
+      } else if (scope.carta.categoria == "Financeiro") {
         scope.carta.classe = 'financeiro';
-    } else if(scope.carta.categoria == "Profissional"){
+      } else if (scope.carta.categoria == "Profissional") {
         scope.carta.classe = 'profissional';
-    } else if(scope.carta.categoria == "Psicológico"){
+      } else if (scope.carta.categoria == "Psicológico") {
         scope.carta.classe = 'psicologico';
-    } else if(scope.carta.categoria == "Relações"){
+      } else if (scope.carta.categoria == "Relações") {
         scope.carta.classe = 'relacoes';
-    } else if(scope.carta.categoria == "Ética"){
+      } else if (scope.carta.categoria == "Ética") {
         scope.carta.classe = 'etica';
-    } else {
-      console.log('erro')
-    }
-    scope.mostraCarta = true;
-
-    if(scope.carta){
+      } else {
+        console.log('erro')
+      }
+      
+      scope.mostraCarta = true;
       scope.removeDoBaralho(scope.carta)
     } else {
       scope.fimDeJogo = true;
